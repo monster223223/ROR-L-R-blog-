41b28ccab11875560062db12e0b12663fa0bdcf0
monster223223 <richdev0223@gmail.com>
2023-02-27 09:02:54 -0800

    blog

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+def index
+    @users = User.all
+  end
+
+  def show
+    @users = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(article_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles
+  resources: users
+  
+end
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
